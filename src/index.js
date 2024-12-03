5375b84e528916d8b45da324351766b6f42e24fb
viviciu <vstaicu@andrew.cmu.edu>
2024-12-03 17:04:28 -0500

    uploaded images and altered zoom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import ForceGraph2D from "react-force-graph-2d";
 import "./index.css";
@@ -58,9 +58,49 @@ const nodePointerAreaPaint = (node, color, ctx) => {
     );
 };
 
+const FocusGraph = () => {
+  const fgRef = useRef();
+
+  useEffect(() => {
+    if (fgRef.current) {
+      fgRef.current.zoom(4); // Set the initial zoom level (2x zoom)
+    }
+  }, []);
+
+  return (
+    <ForceGraph2D
+      ref={fgRef}
+      backgroundColor="#ffffff"
+      graphData={data}
+      nodeLabel={(node) => (node.group !== "TRACK" ? `${node.id}` : "")} // SENIOR QUOTES: set node.description. NAMES: set node.id. if node is not a track
+      linkColor={() => "black"}
+      onNodeClick={(node) => {
+        if (node.url) {
+          window.open(node.url, "_blank");
+        }
+      }}
+      nodeCanvasObject={(node, ctx, globalScale) => {
+        nodePaint(node, ctx, globalScale);
+        nodeLabelPaint(node, ctx, globalScale); // labels underneath items
+      }}
+      nodePointerAreaPaint={nodePointerAreaPaint}
+      d3Force="charge"
+      d3AlphaDecay={0.05}
+      d3VelocityDecay={0.2}
+      d3ForceCharge={(charge) => {
+        charge.strength(-30); // Adjust this value to reduce the repelling force
+      }}
+      d3ForceLink={(link) => {
+        link.distance = 50; // Adjust this value to change the link length
+      }}
+    />
+  );
+};
+
 root.render(
   <React.StrictMode>
-    <ForceGraph2D
+    <FocusGraph />
+    {/* <ForceGraph2D
       backgroundColor="#ffffff"
       graphData={data}
       nodeLabel={(node) => (node.group !== "TRACK" ? `${node.id}` : "")} // SENIOR QUOTES: set node.description. NAMES: set node.id. if node is not a track
@@ -81,7 +121,7 @@ root.render(
       d3ForceLink={(link) => {
         link.distance = 100; // Adjust this value to change the link length
       }}
-    />
+    /> */}
   </React.StrictMode>
 );
 
